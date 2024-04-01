2181c98fa662bacc18948975758767da0f120c02
GlebNevolin <dedalnev@gmail.com>
2024-04-02 03:00:43 +0500

    Added GN fabric

--- base_class/Classes/Classes/EXT4.cpp
+++ base_class/Classes/Classes/EXT4.cpp
@@ -6,20 +6,32 @@
 #include <cmath>
 //Реализации виртуальных методов, описанных в базовом классе---------------------//
 //-------------------------------------------------------------------------------//
-int EXT4::getClusterSize() {
+int EXT4::getClusterInfo() {
 	SetFilePointer(file_handle, 1024, 0, FILE_BEGIN);
 	BYTE* read_buffer = new BYTE[sector_size];
 	ReadFile(file_handle, read_buffer, sector_size, NULL, NULL);
+	//unsigned __int64 LB_power = ReadBytes(read_buffer, 24, 4);
 
-	unsigned __int64 LB_power = ReadBytes(read_buffer, 24, 4);
+#pragma pack(push, 1)
+	struct EXT4_BOOT_REC {
+		BYTE jump1[0x4];
+		UINT32 block_count;
+		BYTE jump2[0x10];
+		UINT32 LB_power;
+	};
+#pragma pack(pop)
 
-	cluster_size = (int)pow(2, (10 + LB_power));
+	EXT4_BOOT_REC* A = new EXT4_BOOT_REC;
+	A = (EXT4_BOOT_REC*)read_buffer;
 
-	delete[] read_buffer;
-	return cluster_size;
+	this->cluster_count = A->block_count;
+	this->cluster_size = (int)pow(2, (10 + A->LB_power));
+
+	delete A;
+	return 0;
 }
 void EXT4::getClusterData(
-	unsigned int cluster_number,
+	ULONGLONG cluster_number,
 	BYTE* read_buffer)
 {
 	unsigned __int64 startOffset = static_cast<unsigned long long>(cluster_number) * cluster_size;
@@ -33,8 +45,8 @@ void EXT4::getClusterData(
 //-------------------------------------------------------------------------------//
 EXT4::EXT4(WCHAR* file_path)
 {
-	cluster_size = NULL;
-	file_handle = CreateFileW(
+	this->cluster_size = NULL;
+	this->file_handle = CreateFileW(
 		file_path,
 		GENERIC_READ | GENERIC_WRITE,
 		FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -43,11 +55,12 @@ EXT4::EXT4(WCHAR* file_path)
 		0,
 		NULL);
 	// Обработчик ошибок при открытии логического диска //
-	if (file_handle == INVALID_HANDLE_VALUE)
+	if (this->file_handle == INVALID_HANDLE_VALUE)
 	{
 		std::cout << "Can't open file with error code: " << GetLastError() << '\n';
-		file_handle = NULL;
+		this->file_handle = NULL;
 	}
+	this->getClusterInfo();
 }
 EXT4::~EXT4()
 {
